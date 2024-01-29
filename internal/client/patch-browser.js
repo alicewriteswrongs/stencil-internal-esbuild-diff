c017471e10ca05e770d258ee8aba1625545d7864
Alice Pote <alice.writes.wrongs@gmail.com>
2024-01-29 14:14:46 -0500

    Revert "back to esbuild"
    This reverts commit 4dc0c5287d5705502ed43667d4918812f632bbfd.

--- internal/client/patch-browser.js
+++ internal/client/patch-browser.js
@@ -1,241 +1,21 @@
 /*
- Stencil Client Patch Browser v4.12.0-dev.1706554080.8760468 | MIT Licensed | https://stenciljs.com
+ Stencil Client Patch Browser v4.12.0-dev.1706552859.8760468 | MIT Licensed | https://stenciljs.com
  */
-var __defProp = Object.defineProperty;
-var __getOwnPropNames = Object.getOwnPropertyNames;
-var __glob = (map2) => (path) => {
-  var fn = map2[path];
-  if (fn) return fn();
-  throw new Error("Module not found in bundle: " + path);
-};
-var __esm = (fn, res) =>
-  function __init() {
-    return fn && (res = (0, fn[__getOwnPropNames(fn)[0]])((fn = 0))), res;
-  };
-var __export = (target, all) => {
-  for (var name in all)
-    __defProp(target, name, { get: all[name], enumerable: true });
-};
-
-// src/utils/regular-expression.ts
-var init_regular_expression = __esm({
-  "src/utils/regular-expression.ts"() {
-    "use strict";
-  },
-});
-
-// src/app-data/index.ts
-var BUILD = {
-  allRenderFn: false,
-  cmpDidLoad: true,
-  cmpDidUnload: false,
-  cmpDidUpdate: true,
-  cmpDidRender: true,
-  cmpWillLoad: true,
-  cmpWillUpdate: true,
-  cmpWillRender: true,
-  connectedCallback: true,
-  disconnectedCallback: true,
-  element: true,
-  event: true,
-  hasRenderFn: true,
-  lifecycle: true,
-  hostListener: true,
-  hostListenerTargetWindow: true,
-  hostListenerTargetDocument: true,
-  hostListenerTargetBody: true,
-  hostListenerTargetParent: false,
-  hostListenerTarget: true,
-  member: true,
-  method: true,
-  mode: true,
-  observeAttribute: true,
-  prop: true,
-  propMutable: true,
-  reflect: true,
-  scoped: true,
-  shadowDom: true,
-  slot: true,
-  cssAnnotations: true,
-  state: true,
-  style: true,
-  formAssociated: false,
-  svg: true,
-  updatable: true,
-  vdomAttribute: true,
-  vdomXlink: true,
-  vdomClass: true,
-  vdomFunctional: true,
-  vdomKey: true,
-  vdomListener: true,
-  vdomRef: true,
-  vdomPropOrAttr: true,
-  vdomRender: true,
-  vdomStyle: true,
-  vdomText: true,
-  watchCallback: true,
-  taskQueue: true,
-  hotModuleReplacement: false,
-  isDebug: false,
-  isDev: false,
-  isTesting: false,
-  hydrateServerSide: false,
-  hydrateClientSide: false,
-  lifecycleDOMEvents: false,
-  lazyLoad: false,
-  profile: false,
-  slotRelocation: true,
-  // TODO(STENCIL-914): remove this option when `experimentalSlotFixes` is the default behavior
-  appendChildSlotFix: false,
-  // TODO(STENCIL-914): remove this option when `experimentalSlotFixes` is the default behavior
-  cloneNodeFix: false,
-  hydratedAttribute: false,
-  hydratedClass: true,
-  scriptDataOpts: false,
-  // TODO(STENCIL-914): remove this option when `experimentalSlotFixes` is the default behavior
-  scopedSlotTextContentFix: false,
-  // TODO(STENCIL-854): Remove code related to legacy shadowDomShim field
-  shadowDomShim: false,
-  // TODO(STENCIL-914): remove this option when `experimentalSlotFixes` is the default behavior
-  slotChildNodesFix: false,
-  invisiblePrehydration: true,
-  propBoolean: true,
-  propNumber: true,
-  propString: true,
-  constructableCSS: true,
-  cmpShouldUpdate: true,
-  devTools: false,
-  shadowDelegatesFocus: true,
-  initializeNextTick: false,
-  asyncLoading: false,
-  asyncQueue: false,
-  transformTagName: false,
-  attachStyles: true,
-  // TODO(STENCIL-914): remove this option when `experimentalSlotFixes` is the default behavior
-  experimentalSlotFixes: false,
-};
-var NAMESPACE =
-  /* default */
-  "app";
-
-// src/client/client-build.ts
-var Build = {
-  isDev: BUILD.isDev ? true : false,
-  isBrowser: true,
-  isServer: false,
-  isTesting: BUILD.isTesting ? true : false,
-};
-
-// src/utils/index.ts
-init_regular_expression();
-
-// src/utils/result.ts
-var result_exports = {};
-__export(result_exports, {
-  err: () => err,
-  map: () => map,
-  ok: () => ok,
-  unwrap: () => unwrap,
-  unwrapErr: () => unwrapErr,
-});
-var ok = (value) => ({
-  isOk: true,
-  isErr: false,
-  value,
-});
-var err = (value) => ({
-  isOk: false,
-  isErr: true,
-  value,
-});
-function map(result, fn) {
-  if (result.isOk) {
-    const val = fn(result.value);
-    if (val instanceof Promise) {
-      return val.then((newVal) => ok(newVal));
-    } else {
-      return ok(val);
-    }
-  }
-  if (result.isErr) {
-    const value = result.value;
-    return err(value);
-  }
-  throw "should never get here";
-}
-var unwrap = (result) => {
-  if (result.isOk) {
-    return result.value;
-  } else {
-    throw result.value;
-  }
-};
-var unwrapErr = (result) => {
-  if (result.isErr) {
-    return result.value;
-  } else {
-    throw result.value;
-  }
-};
-
-// src/runtime/vdom/set-accessor.ts
-var CAPTURE_EVENT_SUFFIX = "Capture";
-var CAPTURE_EVENT_REGEX = new RegExp(CAPTURE_EVENT_SUFFIX + "$");
-
-// src/runtime/dom-extras.ts
-import { NODE_TYPES } from "@stencil/core/mock-doc";
-
-// src/client/client-host-ref.ts
-var hostRefs = BUILD.hotModuleReplacement
-  ? window.__STENCIL_HOSTREFS__ ||
-    (window.__STENCIL_HOSTREFS__ = /* @__PURE__ */ new WeakMap())
-  : /* @__PURE__ */ new WeakMap();
-
-// src/client/client-log.ts
-var STENCIL_DEV_MODE = BUILD.isTesting
-  ? ["STENCIL:"]
-  : [
-      "%cstencil",
-      "color: white;background:#4c47ff;font-weight: bold; font-size:10px; padding:2px 6px; border-radius: 5px",
-    ];
-var consoleDevInfo = (...m) => console.info(...STENCIL_DEV_MODE, ...m);
-
-// import("./**/*.entry.js*") in src/client/client-load-module.ts
-var globImport_entry_js = __glob({});
-
-// src/client/client-window.ts
-var win = typeof window !== "undefined" ? window : {};
-var doc = win.document || { head: {} };
-var H = win.HTMLElement || class {};
-var supportsShadow =
-  // TODO(STENCIL-854): Remove code related to legacy shadowDomShim field
-  BUILD.shadowDomShim && BUILD.shadowDom
-    ? /* @__PURE__ */ (() =>
-        (doc.head.attachShadow + "").indexOf("[native") > -1)()
-    : true;
-var promiseResolve = (v) => Promise.resolve(v);
-var supportsConstructableStylesheets = BUILD.constructableCSS
-  ? /* @__PURE__ */ (() => {
-      try {
-        new CSSStyleSheet();
-        return typeof new CSSStyleSheet().replaceSync === "function";
-      } catch (e) {}
-      return false;
-    })()
-  : false;
-
-// src/client/client-patch-browser.ts
-var patchBrowser = () => {
+import { BUILD, NAMESPACE } from "@stencil/core/internal/app-data";
+import { consoleDevInfo, doc, promiseResolve, H } from "@stencil/core";
+const patchBrowser = () => {
+  // NOTE!! This fn cannot use async/await!
   if (BUILD.isDev && !BUILD.isTesting) {
     consoleDevInfo("Running in development mode.");
   }
   if (BUILD.cloneNodeFix) {
+    // opted-in to polyfill cloneNode() for slot polyfilled components
     patchCloneNodeFix(H.prototype);
   }
   const scriptElm = BUILD.scriptDataOpts
     ? Array.from(doc.querySelectorAll("script")).find(
         (s) =>
-          new RegExp(`/${NAMESPACE}(\\.esm)?\\.js($|\\?|#)`).test(s.src) ||
+          new RegExp(`\/${NAMESPACE}(\\.esm)?\\.js($|\\?|#)`).test(s.src) ||
           s.getAttribute("data-stencil-namespace") === NAMESPACE,
       )
     : null;
@@ -246,7 +26,7 @@ var patchBrowser = () => {
   }
   return promiseResolve(opts);
 };
-var patchCloneNodeFix = (HTMLElementPrototype) => {
+const patchCloneNodeFix = (HTMLElementPrototype) => {
   const nativeCloneNodeFn = HTMLElementPrototype.cloneNode;
   HTMLElementPrototype.cloneNode = function (deep) {
     if (this.nodeName === "TEMPLATE") {
@@ -256,6 +36,7 @@ var patchCloneNodeFix = (HTMLElementPrototype) => {
     const srcChildNodes = this.childNodes;
     if (deep) {
       for (let i = 0; i < srcChildNodes.length; i++) {
+        // Node.ATTRIBUTE_NODE === 2, and checking because IE11
         if (srcChildNodes[i].nodeType !== 2) {
           clonedNode.appendChild(srcChildNodes[i].cloneNode(true));
         }
