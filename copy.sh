f7c322b993a3f2e0def7d6515d0ec28f82bc7389
Alice Pote <alice.writes.wrongs@gmail.com>
2024-02-05 16:30:20 -0500

    make this just more robust I guess

--- copy.sh
+++ copy.sh
@@ -1,8 +1,31 @@
 #!/bin/bash
 
-rm -rf internal
-cp -r ~/Code/stencil/internal .
-npm run prettier
+set -eux
+
+# you need to create a `.env` with the path to your Stencil directory
+# should look like `export STENCIL_DIR=/path/to/repo`
+source .env
+
+# clear out old stuff
+rm -rf internal/rollup/*
+rm -rf internal/esbuild/*
+
+# grab cwd for later
+REPO_DIR=$(pwd)
+
+# Rollup build
+cd $STENCIL_DIR
+npm run clean && npm run build
+
+cd $REPO_DIR
+cp -r $STENCIL_DIR/internal ./internal/rollup/
+
+# Esbuild build
+cd $STENCIL_DIR
+npm run clean && run build.esbuild
+
+cd $REPO_DIR
+cp -r $STENCIL_DIR/internal ./internal/esbuild/
 
 ./strip-comments.mjs
 npm run prettier
