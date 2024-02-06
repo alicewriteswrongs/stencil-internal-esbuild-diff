295cf7f9105335b14e3e20cd09fae4fb02e35203
Alice Pote <alice.writes.wrongs@gmail.com>
2024-02-06 13:19:38 -0500

    back to rollup build

--- internal/app-data/index.cjs
+++ internal/app-data/index.cjs
@@ -1,33 +1,5 @@
 "use strict";
-var __defProp = Object.defineProperty;
-var __getOwnPropDesc = Object.getOwnPropertyDescriptor;
-var __getOwnPropNames = Object.getOwnPropertyNames;
-var __hasOwnProp = Object.prototype.hasOwnProperty;
-var __export = (target, all) => {
-  for (var name in all)
-    __defProp(target, name, { get: all[name], enumerable: true });
-};
-var __copyProps = (to, from, except, desc) => {
-  if ((from && typeof from === "object") || typeof from === "function") {
-    for (let key of __getOwnPropNames(from))
-      if (!__hasOwnProp.call(to, key) && key !== except)
-        __defProp(to, key, {
-          get: () => from[key],
-          enumerable: !(desc = __getOwnPropDesc(from, key)) || desc.enumerable,
-        });
-  }
-  return to;
-};
-var __toCommonJS = (mod) =>
-  __copyProps(__defProp({}, "__esModule", { value: true }), mod);
-var app_data_exports = {};
-__export(app_data_exports, {
-  BUILD: () => BUILD,
-  Env: () => Env,
-  NAMESPACE: () => NAMESPACE,
-});
-module.exports = __toCommonJS(app_data_exports);
-var BUILD = {
+const BUILD = {
   allRenderFn: false,
   cmpDidLoad: true,
   cmpDidUnload: false,
@@ -110,6 +82,8 @@ var BUILD = {
   attachStyles: true,
   experimentalSlotFixes: false,
 };
-var Env = {};
-var NAMESPACE = "app";
-0 && (module.exports = { BUILD: BUILD, Env: Env, NAMESPACE: NAMESPACE });
+const Env = {};
+const NAMESPACE = "app";
+exports.BUILD = BUILD;
+exports.Env = Env;
+exports.NAMESPACE = NAMESPACE;
